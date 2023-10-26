dbf20fab0d9954700668d8e62c51b7eb50c2fd87
TatianaPerezEsquivel <tatianaesquivel233@gmail.com>
2023-10-25 18:02:40 -0600

    upload files

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express' 
+import {config} from 'dotenv'
+config()
+
+import usuariosRoutes from './routes/usuario.router.js'
+
+const PORT = process.env.PORT || 3005;
+
+
+const app = express()
+app.use(express.json())
+
+app.use("/api/usuario",usuariosRoutes)
+
+
+app.listen(PORT,()=>{
+    console.log("😂 Server is running on http://localhost:" + PORT)
+})
